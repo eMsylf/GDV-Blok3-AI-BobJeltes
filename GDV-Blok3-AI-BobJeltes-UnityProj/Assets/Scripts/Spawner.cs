1d962a73637e224f312e512a89830ca521c9b1ce
Bob Jeltes <bobjeltes@hotmail.com>
2019-09-07 15:22:28 +0200

    0.1.7
    - Added Behavior Bricks
    - Started Behavior Bricks tutorial

---
+++ GDV-Blok3-AI-BobJeltes-UnityProj/Assets/Scripts/Spawner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BehaviorBricks {
+    public class Spawner : MonoBehaviour {
+
+        public GameObject prefab;
+        public Vector3 position;
+
+        public GameObject wanderArea;
+        public GameObject player;
+
+        void Start() {
+            GameObject instance = Instantiate(prefab, position, Quaternion.identity) as GameObject;
+            BehaviorExecutor behaviorExecutor = instance.GetComponent<BehaviorExecutor>();
+            if (behaviorExecutor != null) {
+                behaviorExecutor.SetBehaviorParam("wanderArea", wanderArea);
+                behaviorExecutor.SetBehaviorParam("player", player);
+            }
+        }
+
+        void Update() {
+            // hier was ik
+            a;
+        }
+    }
+}
