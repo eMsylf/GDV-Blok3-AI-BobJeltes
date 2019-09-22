5d637526acfe576ee4fe1ef23e2307cf4dfcbcb5
eMsylf <bobjeltes@hotmail.com>
2019-09-22 19:46:14 +0200

    0.2.1
    Experiments start with PandaBT

---
+++ GDV-Blok3-AI-BobJeltes-UnityProj/Assets/HoverBehavior.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using Panda;
+
+public class HoverBehavior : MonoBehaviour
+{
+    [Range(0.1f, .99f)]
+    public float lerpSpeed;
+
+    [Task]
+    private void MoveTo(float xCord, float yCord)
+    {
+        Vector2 goalPosition = new Vector2(xCord, yCord);
+        if (Vector2.Distance(transform.position, goalPosition) > 0f)
+        {
+            transform.position = Vector2.Lerp(transform.position, new Vector2(xCord, yCord), lerpSpeed);
+        } else
+        {
+            Task.current.Succeed();
+        }
+    }
+}
