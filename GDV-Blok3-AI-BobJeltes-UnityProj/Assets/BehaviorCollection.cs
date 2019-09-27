c23922c7cd19e82ec24d147e30008c3385d586ad
Bob Jeltes <bobjeltes@hotmail.com>
2019-09-27 21:53:47 +0200

    0.2.2
    Test with multiple behavior trees

--- GDV-Blok3-AI-BobJeltes-UnityProj/Assets/BehaviorCollection.cs
+++ GDV-Blok3-AI-BobJeltes-UnityProj/Assets/BehaviorCollection.cs
@@ -1,18 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Panda;
 
 public class BehaviorCollection : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    public Transform PlayerArt;
+    public Transform Gun;
+    public Bullet Bullet;
+    public LayerMask EnemyLayer;
+    public int Damage = 1;
+    public float ShotSpeed;
+    private int beamDuration;
+    MeshRenderer HitBoxVisual;
+    //public BoxCollider HitBox;
+    //public int BeamDuration = 3;
+    private Character character;
+    bool isPlayer;
+
+    private void Start() {
+        if (GetComponent<Character>() == null) {
+            isPlayer = false;
+        } else {
+            character = GetComponent<Character>();
+            isPlayer = character.IsPlayer;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+
+    // Ik kan gewoon functionaliteit die ik al gemaakt heb hierin plempen
+    [Task]
+    private void Attack(Transform _target) {
+        transform.LookAt(new Vector3(_target.position.x, transform.position.y, _target.position.z));
     }
 }
